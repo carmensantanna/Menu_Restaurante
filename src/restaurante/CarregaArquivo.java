d738ec68a981d04c62ccbf7605e5942dc17cb93a
501126 <501126@PSG0708.pucsg.net>
2016-05-18 21:02:12 -0300

    Acrescentado comentários e revisão de código

--- src/restaurante/CarregaArquivo.java
+++ src/restaurante/CarregaArquivo.java
@@ -22,23 +22,27 @@ public class CarregaArquivo {
         this.nomeArq = nomeArq;
         menus = new ArrayList();
     }
-    
+    //Lê e carrega os dados do arquivo
     public ArrayList<Menu> leArquivo() {
         try {
+            //Verifica se arquivo existe
             File file = new File(nomeArq);
             if (file.exists()) {
+                //Objeto para leitura
                 FileReader arq = new FileReader(nomeArq);
                 BufferedReader leArq = new BufferedReader(arq);
+                //Variáveis utilizadas
                 Menu menu = new Menu();
                 Prato prato = new Prato();
                 int numDias, quantPrato;
                 double orcamento, custo, lucro;
-
+                //Lê a primeira linha do arquivo
                 String linha = leArq.readLine();
                 String[] parts = linha.split(" ");
                 numDias = Integer.parseInt(parts[0]);
-
+                //Enquanto os dados não forem 0 (encerramento do arquivo)
                 while (numDias != 0) {
+                    //Lê o menu
                     if (parts.length == 3) {
                         quantPrato = Integer.parseInt(parts[1]);
                         orcamento = Double.parseDouble(parts[2]);
@@ -46,6 +50,7 @@ public class CarregaArquivo {
                         menu = new Menu(numDias, quantPrato, orcamento, pratos);
                         menus.add(menu);
                     }
+                    //Lê e carrega os pratos de cada menu
                     if (parts.length == 2) {
                         custo = Double.parseDouble(parts[0]);
                         lucro = Double.parseDouble(parts[1]);
@@ -57,15 +62,17 @@ public class CarregaArquivo {
                     parts = linha.split(" ");
                     numDias = Integer.parseInt(parts[0]);
                 }
-
+                //Fechar o arquivo
+                arq.close();
+                //Retorna os menus carregados
                 return menus;
             }
 
-        } catch (IOException e) {
+        } catch (IOException e) {//Exceção para tratar possíveis erros da abertura do arquivo
             System.err.printf("Erro na abertura do arquivo: %s.\n",
                     e.getMessage());
         }
-
+        //Retorna nulo caso não seja possível ler o arquivo
         return null;
     }
 
--- src/restaurante/MetodoGuloso.java
+++ src/restaurante/MetodoGuloso.java
@@ -13,33 +13,42 @@ public class MetodoGuloso implements IMetodo{
     
     private double lucroMax;
     private double[] razao;
-    
+    //Calcula e retorna o lucro máximo de um determinado menu
     public double calculaLucro(Menu menu)
     {
         double quantOrc = 0;
         lucroMax = 0;
         Prato pratoAtual, ultimoPrato = new Prato();
+        //Pega todos os pratos do menu
         Prato[] pratos = menu.getPratos();
+        //Instancia objeto da classe de ordenação
         QuickSort ordena = new QuickSort();
-        
+        //Para cada dia do cardápio calcula o prato que possui a melhor razão(lucro/custo)
         for(int i = 0; i < menu.getQuantDias(); i++)
         {
+            //Ordena os possíveis pratos do dia pela razão em ordem crescente
             ordena.sort(pratos);
+            //Pega o prato com a maior razão
             pratoAtual = pratos[pratos.length - 1];
+            //Soma o custo do prato no orçamento total
             quantOrc += pratoAtual.getCusto();
-            
+            //Verifica se ainda está dentro do orçamento
             if(quantOrc <= menu.getOrcamento())
             {
+              //Verifica se o prato não está sendo repetido
               if(pratoAtual.getIdPrato() != ultimoPrato.getIdPrato())
                   ultimoPrato.setRepetido(0);
-              
+              //Soma o lucro do prato no lucro total (lucro máximo)
               lucroMax += pratoAtual.getLucroAtual();
+              //Marca que este prato foi utilizado
               pratoAtual.repetePrato();
+              //Guarda o prato atual para verificar se será repetido no próximo dia
               ultimoPrato = pratoAtual;
             }
-            else
+            else //Orçamento estourado
                 lucroMax = 0;
         }
+        //Retorna o lucro máximo calculado 
         return lucroMax;
     }
 }
